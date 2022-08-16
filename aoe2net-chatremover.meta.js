1f012eb446b5135e3797dbb7d155101289c7cbe0
Heistergand <info@heistergand.com>
2022-08-16 23:54:31 +0200

    Create aoe2net-chatremover.meta.js

---
+++ aoe2net-chatremover.meta.js
@@ -0,0 +1,12 @@
+// ==UserScript==
+// @name         AoE2.net sidebar remover
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @downloadURL  https://github.com/Heistergand/fanfields2/raw/master/aoe2net-chatremover.user.js
+// @updateURL    https://github.com/Heistergand/fanfields2/raw/master/aoe2net-chatremover.meta.js
+// @description  Remove that toxic chat
+// @author       You
+// @match        https://aoe2.net/
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=aoe2.net
+// @grant        none
+// ==/UserScript==
